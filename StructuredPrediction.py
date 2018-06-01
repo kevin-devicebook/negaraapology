2c58a8f703f92ca826169198061211d80611648f
kevin-devicebook <kevin@devicebook.com>
2018-05-31 17:40:54 -0700

    Create StructuredPrediction.py

---
+++ StructuredPrediction.py
@@ -0,0 +1,12 @@
+def generate_response(chrisStatement):
+switch(chrisStatement)
+  case "chris mentions jeez":
+   reutrn "the jeez wasnt even what I was upset about"
+  case "chris apologizes again":
+   return "I'm not even looking for an apology"
+  case "chris acknowledges hes a piece of shit":
+   return "I know hes a piece of shit but why cant he just apologize about it"
+  default:
+     log("Man chris is such a fucking piece of shit I knew it this whole time")
+     Thread.sleep(1 month)
+     return "alright chris your good"
